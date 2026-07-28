4bf0771928285263195e34b41fecb83dc9e961a2
mobessir1997 <mobessir1997@gmail.com>
2026-07-28 15:08:16 +0600

    update

--- config/settings.py
+++ config/settings.py
@@ -70,6 +70,14 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES":['rest_framework.permissions.AllowAny'],
     "DEFAULT_PAGINATION_CLASS":"rest_framework.pagination.PageNumberPagination",
     'PAGE_SIZE':20,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/minute',
+        'user': '1000/day'
+    }
 
 }
 SPECTACULAR_SETTINGS = {
